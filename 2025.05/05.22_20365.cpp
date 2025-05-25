b8db65f429f4622b69f8a54e968eaaabcd6d2acc
Kim Dasan <67263093+Plagem@users.noreply.github.com>
2025-05-25 22:45:00 +0900

    S3) 블로그2
    그리디
    연속된 문자열을 하나의 문자로 바꿔도 답이 같다

---
+++ 2025.05/05.22_20365.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string s;
+int n;
+int sol = 1;
+
+
+int main()
+{
+    cin >> n >> s;
+
+    for(int i = 1; i < n; i++)
+    {
+        if(s[i-1] != s[i])
+        {
+            sol++;
+        }
+    }
+
+    cout << sol;
+}
