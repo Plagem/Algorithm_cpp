ad504641980f86ee5b8c2611029df1656dc5f255
Kim Dasan <67263093+Plagem@users.noreply.github.com>
2025-05-28 00:26:24 +0900

    G3) 두 배열의 합
    짬뽕문제
    1. 부분합
    2. 해시맵
    
    롱롱때문에 틀렸다

---
+++ 2025.05/05.28_2143.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <map>
+#include <vector>
+using namespace std;
+
+int t;
+int n, m;
+int sol;
+vector<int> A;
+vector<int> B;
+
+map<int, int> Amap;
+map<int, int> Bmap;
+
+int main()
+{
+    cin >> t;
+    cin >> n;
+
+    A.push_back(0);
+    B.push_back(0);
+
+    for(int i = 1; i <= n; i++)
+    {
+        int t;
+        cin >> t;
+        A.push_back(A[i-1] + t);
+    }
+
+    cin >> m;
+    for(int i = 1; i <= m; i++)
+    {
+        int t;
+        cin >> t;
+        B.push_back(B[i-1] + t);
+    }
+
+    for(int i = 0; i <= n; i++)
+    {
+        for(int j = i+1; j <= n; j++)
+        {
+            int cur = A[j] - A[i];
+            auto iter = Amap.find(cur);
+            if(iter == Amap.end())
+            {
+                Amap.insert({cur, 1});
+            }
+            else
+            {
+                iter->second++;
+            }
+        }
+    }
+
+    for(int i = 0; i <= m; i++)
+    {
+        for(int j = i+1; j <= m; j++)
+        {
+            int cur = B[j] - B[i];
+            auto iter = Bmap.find(cur);
+            if(iter == Bmap.end())
+            {
+                Bmap.insert({cur, 1});
+            }
+            else
+            {
+                iter->second++;
+            }
+        }
+    }
+
+    for(auto iter = Amap.begin(); iter != Amap.end(); iter++)
+    {
+        auto siter = Bmap.find(t-iter->first);
+        if(siter != Bmap.end())
+        {
+            sol += iter->second * siter->second;
+        }
+    }
+
+    cout << sol;
+}
