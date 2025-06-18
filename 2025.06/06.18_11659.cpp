b05658ba8666d34cb4fac42907f90b31b05140d0
Kim Dasan <67263093+Plagem@users.noreply.github.com>
2025-06-18 22:11:28 +0900

    S3) 구간 합 구하기 4
    플러시는 endl이 아니라 \n을 쓰도록 하자

---
+++ 2025.06/06.18_11659.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int n, m;
+vector<int> v;
+
+int main()
+{
+    v.push_back(0);
+    cin >> n >> m;
+    for(int i = 0; i < n; i++)
+    {
+        int t;
+        cin >> t;
+        v.push_back(v[i] + t);
+    }
+
+    for(int i = 0; i < m; i++)
+    {
+        int t1, t2;
+        cin >> t1 >> t2;
+        cout << v[t2] - v[t1-1] << endl;
+    }
+}
